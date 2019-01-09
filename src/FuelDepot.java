4752b6b54b8b3f70482c79e44428852a812ae4f2
BT_1N3_20@bktech.local <BT_1N3_20@bktech.local>
2019-01-09 10:00:12 -0500

    Initial commit

---
+++ src/FuelDepot.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class FuelDepot
+{
+    private FuelRobot filler;
+    private List<FuelTank> tanks;
+
+    public int nextTankToFill(int threshold)
+    {
+        
+    }
+}
